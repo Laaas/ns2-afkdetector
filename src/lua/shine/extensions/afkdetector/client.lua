f0a9355992f0c27b6baa514d57e08ee996268aa6
Las <las@protonmail.ch>
2018-03-18 16:37:46 +0100

    Fix bugs

--- src/lua/shine/extensions/afkdetector/client.lua
+++ src/lua/shine/extensions/afkdetector/client.lua
@@ -19,21 +19,36 @@ function Plugin:Warn()
 	Shine.AddChatText(50, 30, 0, "[AFK]", 1, 1, 0, string.format("Warning! You will be kicked in %i seconds", self.dt.kick_time));
 end
 
-function Plugin:OnThink()
-	local diff = (Shared.GetTime() - last_active_time) / self.dt.spec_mult
-	if warn and diff > self.dt.kick_time then
+function Plugin:Think()
+	local diff = (Shared.GetTime() - last_active_time)
+	if Client.GetLocalPlayer():GetTeamNumber() == kSpectatorIndex then
+		diff = diff / self.dt.spec_mult
+	end
+	if warned and diff > self.dt.kick_time then
 		self:Kick()
-	elseif diff > self.dt._warn_time then
+	elseif diff > self.dt.warn_time then
 		self:Warn()
-		warn = true
+		warned = true
+		last_active_time = Shared.GetTime()
 	end
 end
 
+local old
 local function OnSendKeyEvent(key, down, amount, repeated)
 	if not repeated then
 		last_active_time = Shared.GetTime()
 		warned           = false
 	end
+	old(key, down, amount, repeated)
 end
 
 Event.Hook("SendKeyEvent", OnSendKeyEvent)
+
+local old_hook = Event.Hook
+function Event.Hook(t, f, ...)
+	if t == "SendKeyEvent" then
+		old = f
+	else
+		return old_hook(t, f, ...)
+	end
+end
